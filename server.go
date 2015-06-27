856cde746c9db7ca78efeddd8c13733d822c0854
Andy Haskell <xcrnr2013@gmail.com>
2015-06-27 17:21:18 -0400

    Ran go fmt

--- server.go
+++ server.go
@@ -5,25 +5,24 @@ import (
 	"net/http"
 )
 
-func addStaticRoutes(){
+func addStaticRoutes() {
 	http.Handle("/partials/", http.StripPrefix("/partials/",
-							  http.FileServer(http.Dir("public/partials"))))
+		http.FileServer(http.Dir("public/partials"))))
 	http.Handle("/scripts/", http.StripPrefix("/scripts/",
-							 http.FileServer(http.Dir("public/scripts"))))
+		http.FileServer(http.Dir("public/scripts"))))
 	http.Handle("/styles/", http.StripPrefix("/styles/",
-							http.FileServer(http.Dir("public/styles"))))
+		http.FileServer(http.Dir("public/styles"))))
 	http.Handle("/images/", http.StripPrefix("/images/",
-							http.FileServer(http.Dir("public/images"))))
+		http.FileServer(http.Dir("public/images"))))
 }
 
-func main(){
+func main() {
 	fmt.Println("Initializing database")
 	db := initDB()
 
 	router := initRouter(db)
 	http.Handle("/", router)
 	addStaticRoutes()
-
 
 	fmt.Println("Starting server")
 	http.ListenAndServe(":1123", nil)
