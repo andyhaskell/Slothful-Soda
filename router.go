673968295a2a9db0b510f56055fe15877acaa41e
Andy Haskell <xcrnr2013@gmail.com>
2015-06-27 21:28:36 -0400

    Gave makeLocationsRoute more modularity

--- router.go
+++ router.go
@@ -19,14 +19,24 @@ func indexRoute(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "views/index.html")
 }
 
+//fetchLocations takes in a GORP DbMap and fetches all locations in the
+//locations database table into a Location slice
+func fetchLocations(dbMap *gorp.DbMap) []Location {
+	var locations []Location
+
+	_, err := dbMap.Select(&locations, "SELECT * FROM locations")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	return locations
+}
+
+//makeLocationsRoute takes in a GORP DbMap and makes a route that uses that
+//DbMap to fetch all locations in the locations table and serve them as JSON.
 func makeLocationsRoute(dbMap *gorp.DbMap) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var locations []Location
-
-		_, err := dbMap.Select(&locations, "SELECT * FROM locations")
-		if err != nil {
-			log.Fatal(err)
-		}
+		locations := fetchLocations(dbMap)
 
 		locationsJSON, err := json.Marshal(locations)
 		if err != nil {
@@ -37,20 +47,31 @@ func makeLocationsRoute(dbMap *gorp.DbMap) func(http.ResponseWriter, *http.Reque
 	}
 }
 
-func addStaticRoutes(m *mux.Router, pathsAndDirs ...string) {
+//AddStaticRoutes takes in a Gorilla mux Router and an alternating set of URL
+//paths and directory paths and for each pair of strings, the router is given a
+//FileServer Handler where the first string is the URL path and the second
+//string is the directory path to serve files from.
+func AddStaticRoutes(m *mux.Router, pathsAndDirs ...string) {
 	for i := 0; i < len(pathsAndDirs)-1; i += 2 {
 		FileServerRouteG(m, pathsAndDirs[i], pathsAndDirs[i+1])
 	}
 }
 
+//initRouter takes in a GORP DbMap and initializes the router's routes while
+//using the DbMap to handle database functionality.
 func initRouter(dbMap *gorp.DbMap) *mux.Router {
-	locationsRoute := makeLocationsRoute(dbMap)
-
 	r := mux.NewRouter()
-	addStaticRoutes(r, "/partials/", "public/partials",
+
+	//Add static routes for the public directory
+	AddStaticRoutes(r, "/partials/", "public/partials",
 		"/scripts/", "public/scripts", "/styles/", "public/styles",
 		"/images/", "public/images")
-	r.HandleFunc("/locations", locationsRoute)
+		
+	//Add the locations route API with makeLocationsRoute
+	r.HandleFunc("/locations", makeLocationsRoute(dbMap))
+	
+	//Serve all other requests with index.html, and ultimately the front-end
+	//Angular.js app.
 	r.PathPrefix("/").HandlerFunc(indexRoute)
 
 	return r
